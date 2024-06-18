aadc0bf19ac757e1e01f9ddc8a7ea4f0a3fc57bb
skizodur <agustinramos419@gmail.com>
2024-06-18 12:53:22 -0300

    Deleted

--- src/main/java/org/gamma/buenosayres/dao/interfaces/ResponsableDAO.java
+++
@@ -1,9 +0,0 @@
-package org.gamma.buenosayres.dao.interfaces;
-
-import org.gamma.buenosayres.model.Responsable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ResponsableDAO extends JpaRepository<Responsable, UUID> {
-}
