3645d1ee419bb9c3c049fc7384e36869ec7c0c2b
skizodur <agustinramos419@gmail.com>
2024-06-25 17:35:40 -0300

    added id

--- src/main/java/org/gamma/buenosayres/dto/DetalleFacturaDTO.java
+++ src/main/java/org/gamma/buenosayres/dto/DetalleFacturaDTO.java
@@ -1,42 +1,62 @@
 package org.gamma.buenosayres.dto;
 
 import java.util.Date;
+import java.util.UUID;
 
 public class DetalleFacturaDTO {
-     private ConceptoDTO concepto;
-     private AlumnoDTO alumno;
-     private int descuento;
-     private boolean abonado;
-     private Date fechaPago;
+	private UUID id;
+	private ConceptoDTO concepto;
+	private AlumnoDTO alumno;
+	private int descuento;
+	private boolean abonado;
+	private Date fechaPago;
+
 	public ConceptoDTO getConcepto() {
 		return concepto;
 	}
+
 	public void setConcepto(ConceptoDTO concepto) {
 		this.concepto = concepto;
 	}
+
 	public AlumnoDTO getAlumno() {
 		return alumno;
 	}
+
 	public void setAlumno(AlumnoDTO alumno) {
 		this.alumno = alumno;
 	}
+
 	public int getDescuento() {
 		return descuento;
 	}
+
 	public void setDescuento(int descuento) {
 		this.descuento = descuento;
 	}
+
 	public boolean isAbonado() {
 		return abonado;
 	}
+
 	public void setAbonado(boolean abonado) {
 		this.abonado = abonado;
 	}
+
 	public Date getFechaPago() {
 		return fechaPago;
 	}
+
 	public void setFechaPago(Date fechaPago) {
 		this.fechaPago = fechaPago;
 	}
-     
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
 }
